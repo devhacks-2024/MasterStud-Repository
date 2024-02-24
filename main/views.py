fc69b7352b735c50c906292a247bea3b5177dc12
Minh Phan <minhsg.phan@gmail.com>
2024-02-24 09:40:10 -0600

    store jsonObj locally

--- main/views.py
+++ main/views.py
@@ -3,11 +3,13 @@ from django.template import loader
 from django.shortcuts import render
 import json
 
+jsonObj = {}
 # Create your views here.
 def main(request):
   return render(request, "index.html")
 
 
 def upload_file(request):
+  global jsonObj
   if request.method == "POST":
     jsonObj = json.load(request.FILES["file"])
