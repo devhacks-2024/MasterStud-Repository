9dc38ca5665095ba5d031dc9c280f457c8653e7b
kyuminq <edwardcunningham710@protonmail.com>
2024-02-23 23:51:40 -0600

    chore: commented test data in canvas

--- main/templates/canvas.js
+++ main/templates/canvas.js
@@ -4,56 +4,56 @@ function draw(object) {
     // uncomment the following to test it
     //make sure to remove the object parameter
 
-    const object = {
-        "room_details": {
-          "room": {
-            "coordinates": [
-              {"x": 250, "y": 250},
-              {"x": 500, "y": 500}
-            ]
-          },
-          "window": {
-            "coordinates": [
-              {"x": 0, "y": 3},
-              {"x": 0, "y": 4}
-            ],
-            "furniture": "bed"
-          },
-          "door": {
-            "coordinates": [
-              {"x": 3, "y": 0},
-              {"x": 4, "y": 0}
-            ]
-          },
-          "socket": {
-            "coordinates": [
-              {"x": 5, "y": 2},
-              {"x": 6, "y": 3}
-            ],
-            "furniture": "desk"
-          }
-        },
-        "furniture_details": [
-          {
-            "tag": "bed",
-            "length": 2,
-            "width": 2,
-            "coordinates": [
-              {"x": 250, "y": 250},
-              {"x": 300, "y": 300}
-            ]
-          },
-          {
-            "tag": "bed",
-            "length": 2,
-            "width": 2,
-            "coordinates": [
-              {"x": 350, "y": 350},
-              {"x": 450, "y": 500}
-            ]
-          }
-        ]
-      }
+    // const object = {
+    //     "room_details": {
+    //       "room": {
+    //         "coordinates": [
+    //           {"x": 250, "y": 250},
+    //           {"x": 500, "y": 500}
+    //         ]
+    //       },
+    //       "window": {
+    //         "coordinates": [
+    //           {"x": 0, "y": 3},
+    //           {"x": 0, "y": 4}
+    //         ],
+    //         "furniture": "bed"
+    //       },
+    //       "door": {
+    //         "coordinates": [
+    //           {"x": 3, "y": 0},
+    //           {"x": 4, "y": 0}
+    //         ]
+    //       },
+    //       "socket": {
+    //         "coordinates": [
+    //           {"x": 5, "y": 2},
+    //           {"x": 6, "y": 3}
+    //         ],
+    //         "furniture": "desk"
+    //       }
+    //     },
+    //     "furniture_details": [
+    //       {
+    //         "tag": "bed",
+    //         "length": 2,
+    //         "width": 2,
+    //         "coordinates": [
+    //           {"x": 250, "y": 250},
+    //           {"x": 300, "y": 300}
+    //         ]
+    //       },
+    //       {
+    //         "tag": "bed",
+    //         "length": 2,
+    //         "width": 2,
+    //         "coordinates": [
+    //           {"x": 350, "y": 350},
+    //           {"x": 450, "y": 500}
+    //         ]
+    //       }
+    //     ]
+    //   }
 
     const canvas = document.getElementById("canvas");
     if (canvas.getContext) {
