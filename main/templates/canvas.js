1a898c797b2a07a51582174f025ffc16b3755130
kyuminq <edwardcunningham710@protonmail.com>
2024-02-23 23:43:08 -0600

    feat: draw() for room & furnitures

--- main/templates/canvas.js
+++ main/templates/canvas.js
@@ -1,10 +1,90 @@
-function draw() {
+function draw(object) {
+// function draw() {
+
+    // uncomment the following to test it
+    //make sure to remove the object parameter
+
+    const object = {
+        "room_details": {
+          "room": {
+            "coordinates": [
+              {"x": 250, "y": 250},
+              {"x": 500, "y": 500}
+            ]
+          },
+          "window": {
+            "coordinates": [
+              {"x": 0, "y": 3},
+              {"x": 0, "y": 4}
+            ],
+            "furniture": "bed"
+          },
+          "door": {
+            "coordinates": [
+              {"x": 3, "y": 0},
+              {"x": 4, "y": 0}
+            ]
+          },
+          "socket": {
+            "coordinates": [
+              {"x": 5, "y": 2},
+              {"x": 6, "y": 3}
+            ],
+            "furniture": "desk"
+          }
+        },
+        "furniture_details": [
+          {
+            "tag": "bed",
+            "length": 2,
+            "width": 2,
+            "coordinates": [
+              {"x": 250, "y": 250},
+              {"x": 300, "y": 300}
+            ]
+          },
+          {
+            "tag": "bed",
+            "length": 2,
+            "width": 2,
+            "coordinates": [
+              {"x": 350, "y": 350},
+              {"x": 450, "y": 500}
+            ]
+          }
+        ]
+      }
+
     const canvas = document.getElementById("canvas");
     if (canvas.getContext) {
-      const ctx = canvas.getContext("2d");
-  
-      ctx.fillRect(25, 25, 100, 100);
-      ctx.clearRect(45, 45, 60, 60);
-      ctx.strokeRect(50, 50, 50, 50);
+        const ctx = canvas.getContext("2d");
+    
+        // Draw the room
+        const room = object.room_details.room;
+        const roomCoordinates = room.coordinates;
+        const roomWidth = roomCoordinates[1].x - roomCoordinates[0].x;
+        const roomHeight = roomCoordinates[1].y - roomCoordinates[0].y;
+
+        // const window = object.room_details.window;
+        // const windowCoordinates = window.coordinates;
+        // const windowAdjacentFurnture = window.furniture;
+    
+        // const socket = object.room_details.socket;
+        // const socketCoordinates = socket.coordinates;
+        // const socketAdjacentFurnture = socket.furniture;
+    
+        // const door = object.room_details.door;
+        // const doorCoordinates = door.coordinates;
+
+        ctx.strokeRect(roomCoordinates[0].x, roomCoordinates[0].y, roomWidth, roomHeight);
+        
+        // // Draw the furniture
+        const furnitureDetails = object.furniture_details;
+        furnitureDetails.forEach(furniture => {
+        const coordinates = furniture.coordinates;
+        const width = coordinates[1].x - coordinates[0].x;
+        const height = coordinates[1].y - coordinates[0].y;
+        ctx.strokeRect(coordinates[0].x, coordinates[0].y, width, height);
+        });
     }
-  }
+}
